d3b7572b4538c4bc1d5af1bec27f867c2c4db445
panqinqiang <qinqiangpan@gmail.com>
2023-09-25 23:27:11 +0800

    括号匹配，栈解决

--- MianShiAlgorithmCSharp/_20230925/ValidParentheses.cs
+++ MianShiAlgorithmCSharp/_20230925/ValidParentheses.cs
@@ -2,5 +2,39 @@ namespace MianShiAlgorithmCSharp._20230925;
 
 public class ValidParentheses
 {
+    public static bool IsVaild(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        string openBrackets = "({[";
+        string closeBrackets = ")}]";
+
+        foreach (char c in s)
+        {
+            if (openBrackets.Contains(c))
+            {
+                stack.Push(c);
+            }else if (closeBrackets.Contains(c))
+            {
+                if (stack.Count == 0 || stack.Pop() != GetMatchingOpenBracket(c))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
     
+    private static char GetMatchingOpenBracket(char closeBracket) {
+        switch (closeBracket) {
+            case ')':
+                return '(';
+            case '}':
+                return '{';
+            case ']':
+                return '[';
+            default:
+                return ' ';
+        }
+    }
 }
